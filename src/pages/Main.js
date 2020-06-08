2798eb186b185d096d75ae80546b710064c6387f
rusla <ruslanbekshenev@yandex.ru>
2020-06-09 03:17:44 +0500

    Игра

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,33 +1,23 @@
 import React, {Component} from 'react';
 import "../css/game.css"
 import shirt from "../cards/shirt.png";
+import Stopwatcher from "../components/Stopwatcher";
 
 class Main extends Component {
     state = {
-        size: 6,
+        size: 2,
         table: [],
         cards: [],
         firstCard: null,
         secondCard: null,
-        selectedcards: []
+        isFinished: false,
+        retiredCount: 0,
+        timeResult: null
     }
     shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
     initCards = () => {
         const {size} = this.state;
         const cards = [];
-        // for (let i = 1; i <= Math.pow(size, 2) / 2; i++) {
-        //     for (let j = 0; j < 2; j++) {
-        //         const index = j > 0 ? i+1 : i;
-        //         // console.log(index)
-        //         cards.push({
-        //             index: index,
-        //             number: i,
-        //             rotate: false,
-        //             retired: false,
-        //             img: require(`../cards/front/${i}.png`)
-        //         });
-        //     }
-        // }
         for (let i = 1; i <= Math.pow(size, 2); i++) {
             const valCard = i % 2 === 0 ? i / 2 : (i + 1) / 2;
             cards.push({
@@ -40,22 +30,23 @@ class Main extends Component {
         }
         const cardsShuffle = this.shuffle(cards);
         const matrixCards = [];
-        cardsShuffle.forEach( (item, index, cardsShuffle) => {
+        cardsShuffle.forEach((item, index, cardsShuffle) => {
             matrixCards.push(cardsShuffle.splice(0, size));
         })
         this.setState({
             cards: matrixCards
         })
-        
+
     }
     renderTable = () => {
-        const {cards, size} = this.state
-        return cards.map( (cardsRows, index) => (
+        const {cards, size, isFinished} = this.state
+        return cards.map((cardsRows, index) => (
             <div className="row" key={index}>
                 {
-                    cardsRows.map( (card, childIndex) => (
-                        <div key={index+childIndex} style={ { width: `${(100 / size)}%`} } className="cell">
-                            <button style={{width: "100%", height: "120px"}} className="cell-btn" onClick={ () => this.clickHandler(card) }>
+                    cardsRows.map((card, childIndex) => (
+                        <div key={index + childIndex} style={{width: `${(100 / size)}%`}} className="cell">
+                            <button style={{width: "100%", height: "120px"}} className={!card.retired ? "cell-btn" : "cell-btn retired"}
+                                    onClick={() => this.clickHandler(card)}>
                                 <div className={card.rotate ? "btn-container flip-card" : "btn-container"}>
                                     <div className="card card-shirt">
                                         <img src={shirt} alt=""/>
@@ -66,72 +57,95 @@ class Main extends Component {
                                 </div>
                             </button>
                         </div>
-                    ) )
+                    ))
                 }
+
             </div>
         ))
     }
     clickHandler = (card) => {
         const {firstCard, secondCard, cards} = this.state;
         const newCards = [...cards]
-        if (!firstCard) {
+
+        if (!firstCard || !secondCard) {
             newCards.map( cardRow => {
-                const item = cardRow.find( cardItem => cardItem.index === card.index );
-                if (!!item) {
-                    item.rotate = true
-                }    
-            } )
-            this.setState({
-                firstCard: card,
-            })
-        } 
-        else if (!secondCard) {
-            newCards.map( cardRow => {
-                const item = cardRow.find( cardItem => cardItem.index === card.index );
-                if (!!item) {
-                    item.rotate = true
-                }    
-            } )
-            this.setState({
-                secondCard: card,
+                const selectedCard = cardRow.find(cardItem => cardItem.index === card.index);
+                if (!!selectedCard) {
+                    selectedCard.rotate = true;
+
+                    this.setState({
+                        firstCard: !firstCard ? {...card} : firstCard,
+                        secondCard: !secondCard && firstCard ? {...card} : secondCard,
+                    })
+                }
             })
         }
-       setTimeout(() => {
-            console.log('test')
-            console.log(firstCard, secondCard)
-           if (firstCard && secondCard) {
-               
-            if (firstCard.number === secondCard.number) {
-                console.log('true')
+        setTimeout(() => {
+            const {firstCard, secondCard, cards} = this.state;
+            this.checkFinish();
+            if (firstCard && secondCard) {
+                const modifierCards = firstCard.number === secondCard.number ? [...cards.map( cardRow =>
+                    cardRow.map( cardItem =>
+                        cardItem.index === firstCard.index || cardItem.index === secondCard.index ?
+                            {...cardItem, rotate: true, retired: true} : cardItem )
+                )] : [...cards]
+
+                this.setState({
+                    firstCard: null,
+                    secondCard: null,
+                    cards: [...modifierCards],
+
+                })
             }
-            
-           }
-           newCards.map( cardRow => {
-                const item = cardRow.find( cardItem => cardItem.index === card.index );
-                if (!!item) {
-                    item.rotate = false
-                }    
-            } )
-           this.setState({
-            firstCard: null,
-            secondCard: null,
+            else {
+                this.setState({
+                    firstCard: null,
+                    secondCard: null,
+                    cards: [...cards.map( cardRow => cardRow.map( cardItem => !cardItem.retired ?
+                        {...cardItem, rotate: false} :
+                        {...cardItem, rotate: true} )
+                    )]
+                })
+            }
+        }, 5000)
+    }
+    checkFinish = async () => {
+        const {cards, size, retiredCount} = this.state;
+        let count = 0;
+        cards.map( cardRow => {
+            cardRow.map( cardItem => cardItem.retired ? count++ : count  )
+        } )
+        console.log(Math.pow(size, 2), count);
+        this.setState({
+            isFinished: Math.pow(size, 2) === count
         })
-       }, 2000)
+    }
+    setTimeResult = (time) => {
+        this.setState({
+            timeResult: time
+        })
     }
     componentDidMount() {
         this.initCards();
     }
 
     render() {
+        const {isFinished, timeResult} = this.state
         return (
             <div className="game-container">
                 <div className="game-block">
                     <div className="game-container__header">
-                        <h2>Game "Memory"</h2>
+                        <div className={"space-between"}>
+                            <h2>Memory game</h2>
+                            <Stopwatcher stopWatch={isFinished} setTimeResult={this.setTimeResult}/>
+                        </div>
                     </div>
                     <div className="game-container__body">
                         <div className="table">
                             {this.renderTable()}
+                        </div>
+                        <div className={isFinished ? "finish" : "hidden"}>
+                            Your result: {timeResult}
                         </div>
                     </div>
                 </div>
