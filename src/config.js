a92f784441362d54a6ed352f1f444be21a65fc66
rusla <ruslanbekshenev@yandex.ru>
2020-06-11 02:57:07 +0500

    рефакторинг

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    apiKey: "{YOUR KEY}",
-    authDomain: "{YOUR KEY}",
-    databaseURL: "{YOUR KEY}",
-  }
-  
-  export default config;
+    apiKey: "AIzaSyAHMtTV3kQM3-sgWpGUpLKencXGFeG_0lo",
+    authDomain: "memory-game-bd6fc.firebaseapp.com",
+    databaseURL: "https://memory-game-bd6fc.firebaseio.com",
+}
+
+export default config;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,14 @@
-import React, {Component} from 'react';
-import "../css/game.css"
-import shirt from "../cards/shirt.png";
-import Stopwatcher from "../components/Stopwatcher";
+import React, { Component } from 'react'
+import '../css/game.css'
+import shirt from '../cards/shirt.png'
+import Stopwatcher from '../components/Stopwatcher'
+import cn from 'classnames'
+import { debounce } from '../utils/debounce'
+
+/**
+ * Время переворота карточек
+ */
+const TIME_OF_REVERCE = 2000
 
 class Main extends Component {
     state = {
@@ -12,145 +19,233 @@ class Main extends Component {
         secondCard: null,
         isFinished: false,
         retiredCount: 0,
-        timeResult: null
+        timeResult: null,
+        // Свойство, необходимое для продления открытия первой карты после открытия второй карты
+        prolongate: false,
     }
-    shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+
+    /**
+     * Поле под таймаут
+     */
+    timeout = null
+
+    /**
+     * @param arr [] - Массив всех кард
+     * @returns [] - Возвращает новый массив состоящий из тех же карт но перемешаный. Не мутирует.
+     */
+    shuffle = (arr) => arr.sort(() => Math.random() - 0.5)
+
     initCards = () => {
-        const {size} = this.state;
-        const cards = [];
+        const { size } = this.state
+        const cards = []
         for (let i = 1; i <= Math.pow(size, 2); i++) {
-            const valCard = i % 2 === 0 ? i / 2 : (i + 1) / 2;
+            // Число, по которому будем сразвнивать карты
+            const valCard = i % 2 === 0 ? i / 2 : (i + 1) / 2
             cards.push({
-                index: i % 2 === 0 ? i : i,
+                index: i,
                 number: valCard,
-                rotate: false,
                 retired: false,
-                img: require(`../cards/front/${valCard}.png`)
-            });
+                img: require(`../cards/front/${valCard}.png`),
+            })
         }
-        const cardsShuffle = this.shuffle(cards);
-        const matrixCards = [];
+        const cardsShuffle = this.shuffle(cards)
+        const matrixCards = []
         cardsShuffle.forEach((item, index, cardsShuffle) => {
-            matrixCards.push(cardsShuffle.splice(0, size));
+            matrixCards.push(cardsShuffle.splice(0, size))
         })
         this.setState({
-            cards: matrixCards
+            cards: matrixCards,
         })
-
     }
+
+    /**
+     * Метод который проверяет, перевернута эта карта или нет
+     * @param Object card
+     * @returns boolean
+     */
+    checkIfRotated = (card) => {
+        const { firstCard, secondCard } = this.state
+        const output =
+            (firstCard && firstCard.index === card.index) ||
+            (secondCard && secondCard.index === card.index) ||
+            card.retired
+        return output
+    }
+
+    /**
+     * рендерит таблицу с картами
+     */
     renderTable = () => {
-        const {cards, size} = this.state
+        const { checkIfRotated } = this
+        const { cards, size } = this.state
         return cards.map((cardsRows, index) => (
             <div className="row" key={index}>
-                {
-                    cardsRows.map((card, childIndex) => (
-                        <div key={index + childIndex} style={{width: `${(100 / size)}%`}} className="cell">
-                            <button style={{width: "100%", height: "120px"}} className={!card.retired ? "cell-btn" : "cell-btn retired"}
-                                    onClick={() => this.clickHandler(card)}>
-                                <div className={card.rotate ? "btn-container flip-card" : "btn-container"}>
-                                    <div className="card card-shirt">
-                                        <img src={shirt} alt=""/>
-                                    </div>
-                                    <div className="card card-face">
-                                        <img src={card.img} alt=""/>
-                                    </div>
+                {cardsRows.map((card, childIndex) => (
+                    <div
+                        key={index + childIndex}
+                        style={{ width: `${100 / size}%` }}
+                        className="cell"
+                    >
+                        <button
+                            style={{ width: '100%', height: '120px' }}
+                            className={cn('cell-btn', {
+                                retired: !card.retired,
+                            })}
+                            onClick={() => this.clickHandler(card)}
+                        >
+                            <div
+                                className={cn('btn-container', {
+                                    'flip-card': checkIfRotated(card),
+                                })}
+                            >
+                                <div className="card card-shirt">
+                                    <img src={shirt} alt="" />
                                 </div>
-                            </button>
-                        </div>
-                    ))
-                }
-
+                                <div className="card card-face">
+                                    <img src={card.img} alt="" />
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                ))}
             </div>
         ))
     }
+
+    /**
+     *
+     */
     clickHandler = async (card) => {
         const newCards = [...this.state.cards]
-        if (!this.state.firstCard || !this.state.secondCard) {
-            await newCards.map( cardRow => {
-                const selectedCard = cardRow.find(cardItem => cardItem.index === card.index);
-                if (!!selectedCard) {
-                    selectedCard.rotate = true;
+        const { state, timeout } = this
+        let { firstCard, secondCard, cards } = state
+
+        if (!firstCard || !secondCard) {
+            if (!firstCard) {
+                firstCard = card
+            } else {
+                // Экранируем даблклик по карточке
+                if (card.index === firstCard.index) return
+                secondCard = card
+            }
+
+            newCards.forEach((cardRow) => {
+                const selectedCard = cardRow.find(
+                    (cardItem) => cardItem.index === card.index
+                )
+                if (selectedCard) {
                     this.setState({
-                        firstCard: !this.state.firstCard ? {...card} : this.state.firstCard,
-                        secondCard: !this.state.secondCard && this.state.firstCard && this.state.firstCard.index !== card.index ? {...card} : this.state.secondCard
+                        prolongate: true,
+                        firstCard: firstCard,
+                        secondCard: secondCard,
                     })
                 }
-            } );
-        }
-        if (this.state.firstCard && this.state.secondCard) {
-            const modifierCards = await this.state.firstCard.number === this.state.secondCard.number ?
-                [...this.state.cards.map( cardRow => cardRow.map( cardItem =>
-                    (cardItem.index === this.state.firstCard.index || cardItem.index === this.state.secondCard.index) ?
-                        {...cardItem, rotate: true, retired: true} : cardItem )
-                )] : [...this.state.cards]
-
-            this.setState({
-                cards: [...modifierCards],
             })
-
-
-
-            this.checkFinish();
-        }
-        console.log(this.state.firstCard, this.state.secondCard);
-        if (!this.state.secondCard) {
-            setTimeout(() => {
-                this.setState({
-                    firstCard: null,
-                    secondCard: null,
-                    cards: [...this.state.cards.map( cardRow => cardRow.map( cardItem => !cardItem.retired ?
-                        {...cardItem, rotate: false} :
-                        {...cardItem, rotate: true} )
-                    )]
-                })
-            }, 2000)
         }
 
+        if (firstCard && secondCard) {
+            const success = firstCard.number === secondCard.number
+
+            if (success && timeout) {
+                clearTimeout(timeout)
+
+                this.setState(
+                    {
+                        firstCard: null,
+                        secondCard: null,
+                        cards: [
+                            ...cards.map((cardRow) =>
+                                cardRow.map((cardItem) =>
+                                    cardItem.index === firstCard.index ||
+                                    cardItem.index === secondCard.index
+                                        ? {
+                                            ...cardItem,
+                                            retired: true,
+                                        }
+                                        : cardItem
+                                )
+                            ),
+                        ],
+                    },
+                    this.checkFinish
+                )
+            }
+        }
+
+        if (!secondCard) {
+            this.resetCards()
+        }
     }
-    checkFinish = async () => {
-        const {cards, size} = this.state;
-        let count = 0;
-        cards.map( cardRow => {
-            cardRow.map( cardItem => cardItem.retired ? count++ : count  )
-        } )
+
+    /**
+     * Перевернуть все карты обратно рубашкой вверх. Метод задекорировать под Debounce
+     */
+    resetCards = debounce(() => {
+        const { prolongate } = this.state
+
+        this.timeout = setTimeout(() => {
+            if (prolongate) {
+                this.setState({ prolongate: false })
+                this.resetCards()
+                return
+            }
+            this.setState({
+                firstCard: null,
+                secondCard: null,
+            })
+        }, TIME_OF_REVERCE)
+    }, TIME_OF_REVERCE)
+
+    checkFinish = () => {
+        const { size, cards } = this.state
+        let count = 0
+        cards.forEach((cardRow) => {
+            cardRow.forEach((cardItem) => {
+                if (cardItem.retired) {
+                    count++
+                }
+            })
+        })
+
         this.setState({
-            isFinished: Math.pow(size, 2) === count
+            isFinished: Math.pow(size, 2) === count,
         })
     }
     setTimeResult = (time) => {
         this.setState({
-            timeResult: time
+            timeResult: time,
         })
     }
     componentDidMount() {
-        this.initCards();
+        this.initCards()
     }
 
     render() {
-        console.log(this.props.location.state.nick)
-        const {isFinished, timeResult} = this.state
+        const { isFinished, timeResult } = this.state
+        const { location } = this.props
+
         return (
             <div className="container">
                 <div className="game-block">
                     <div className="game-container__header">
-                        <div className={"space-between"}>
+                        <div className={'space-between'}>
                             <h2>Memory game</h2>
-                            <Stopwatcher stopWatch={isFinished} setTimeResult={this.setTimeResult}/>
+                            <Stopwatcher
+                                stopWatch={isFinished}
+                                setTimeResult={this.setTimeResult}
+                            />
                         </div>
                     </div>
                     <div className="game-container__body">
-                        <div className="table">
-                            {this.renderTable()}
-                        </div>
-                        <div className={isFinished ? "finish" : "hidden"}>
-                        {this.props.location.state.nick}, your result: {timeResult}
+                        <div className="table">{this.renderTable()}</div>
+                        <div className={isFinished ? 'finish' : 'hidden'}>
+                            {location.state.nick}, your result: {timeResult}
                         </div>
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 }
 
-
-export default Main;
+export default Main
